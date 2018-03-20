d44273fd21e5cf334db4759021f61c87be26f79b
jondon94 <jondonaldson94@gmail.com>
2018-03-20 20:46:31 +0000

    added public class

--- AdvCal.java
+++ AdvCal.java
@@ -1,3 +1,9 @@
 //This project is for making a calculator that can handle multiple number sna d operands and compute them in the correct (BIDMAS) sequence.
+//Use of arrays for storing numbers in sequence, a for addition, s for subtraction, m for mulitplcation, d for division and 'end' for ending the sequence and calculating
 
 import java.util.*;
+import java.math.*;
+
+public class AdvCal {
+
+}
