18819decae64cc5f8267e96f1b71eff75bdde647
jondon94 <jondonaldson94@gmail.com>
2018-03-20 22:12:18 +0000

    intro updated, regex will need to be implemented

--- AdvCal.java
+++ AdvCal.java
@@ -1,5 +1,6 @@
 //This project is for making a calculator that can handle multiple number sna d operands and compute them in the correct (BIDMAS) sequence.
 //Use of arrays for storing numbers in sequence, a for addition, s for subtraction, m for mulitplcation, d for division and 'end' for ending the sequence and calculating
+//This projject is more difficylt than intitally imagined. Therefore REGEX will need to be used for digit replacment and character substitution 
 
 import java.util.*;
 import java.math.*;
@@ -8,5 +9,7 @@ import java.util.Scanner;
 public class AdvCal {
   public static void main(String[] args){
     System.out.println("Welcome to the Advanced Calculator. This calculator will compute multiple number using multiple operands in the correct order (BIDMAS). Exponents and roots will be used.");
+
+
   }
 }
